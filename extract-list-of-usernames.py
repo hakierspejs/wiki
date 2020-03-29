fb881de70bb20badc01a79878363f3314762a7eb
Jacek Wielemborek <d33tah@gmail.com>
2020-03-29 06:59:32 +0200

    Actually generate a markdown file, look at all pages

--- extract-list-of-usernames.py
+++ extract-list-of-usernames.py
@@ -16,18 +16,25 @@ def main():
     if not pathlib.Path('wiki.wiki').exists():
         subprocess.call(['git', 'clone', WIKI_URL])
     markdown = mistune.create_markdown(renderer=mistune.AstRenderer())
-    with open('wiki.wiki/Infrastruktura.md') as f:
-        parsed = markdown(f.read())
-    last_heading = None
     projects_per_user = collections.defaultdict(list)
-    for entry in parsed:
-        if entry['type'] == 'heading':
-            last_heading = entry['children'][0]['text']
-        else:
-            for child in entry.get('children', []):
-                for user in re.findall('@\w+', child.get('text', '')):
-                    projects_per_user[user].append(last_heading)
-    pprint.pprint(dict(projects_per_user))
+    for fname in pathlib.Path('wiki.wiki').glob('**/*.md'):
+        with open(fname) as f:
+            parsed = markdown(f.read())
+        ftitle = '/'.join([
+            x[:-3] if x.endswith('.md') else x for x in fname.parts[1:]
+        ])
+        title = None
+        for entry in parsed:
+            if entry['type'] == 'heading':
+                title = f"{ftitle}#{entry['children'][0]['text']}"
+            else:
+                for child in entry.get('children', []):
+                    for user in re.findall('@\w+', child.get('text', '')):
+                        projects_per_user[user[1:]].append(title)
+    for user in sorted(projects_per_user, key=lambda x: x.lower()):
+        print(f'\n# {user}\n')
+        for project in sorted(projects_per_user[user]):
+            print(f' * [[{project}]]')
 
 
 if __name__ == '__main__':
